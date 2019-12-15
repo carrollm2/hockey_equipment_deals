7f4525831686a5496101fe95b5908cbfcf47b9c5
Michael Carroll <carrollm2@gmail.com>
2019-12-15 02:06:08 +0000

    create module HockeyDeal to be used as base class for types of hockey equipment deals

---
+++ lib/hockey_deal.rb
@@ -0,0 +1,19 @@
+module HockeyDeal
+
+  module InstanceMethods
+
+    def set_name(hockey_deal_hash)
+      @name = hockey_deal_hash[:name]
+    end
+
+    def set_description_link(hockey_deal_hash)
+      @description_link = hockey_deal_hash[:description_link]
+    end
+
+    def set_store(store_name)
+      @store = store_name
+    end
+    
+  end
+
+end
