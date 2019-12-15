3f47c7c4266617e03eba6dbd413798b18328189b
Michael Carroll <carrollm2@gmail.com>
2019-12-15 05:32:55 +0000

    added Instance Methods to HockeyEquipmentDeals module

--- lib/hockey_equipment_deals.rb
+++ lib/hockey_equipment_deals.rb
@@ -1,6 +1,22 @@
-require "hockey_equipment_deals/version"
+require_relative "hockey_equipment_deals/version"
+require 'pry'
 
 module HockeyEquipmentDeals
   class Error < StandardError; end
-  # Your code goes here...
+    
+  module InstanceMethods
+
+    def set_name(hockey_deal_hash)
+      @name = hockey_deal_hash[:name]
+    end
+
+    def set_description_link(hockey_deal_hash)
+      @description_link = hockey_deal_hash[:description_link]
+    end
+
+    def set_store(store_name)
+      @store = store_name
+    end
+    
+  end
 end
