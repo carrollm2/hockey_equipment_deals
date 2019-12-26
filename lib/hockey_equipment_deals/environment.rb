d72130c180ff6037b4dfc70301a17ef71b6a2ec2
Michael Carroll <carrollm2@gmail.com>
2019-12-26 01:48:31 +0000

    create enviromment file for manage load dependencies

---
+++ lib/hockey_equipment_deals/environment.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../scraper.rb'
+require_relative '../hockey_equipment_deals.rb'
+require_relative '../top_seller.rb'
+require_relative '../clearance.rb'
+require_relative '../menu.rb'
