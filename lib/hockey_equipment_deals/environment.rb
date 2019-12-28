bafae4b18b557aa12ff24bd1f464a7c8b79470ef
Michael Carroll <carrollm2@gmail.com>
2019-12-28 21:18:02 +0000

    moved environment.rb to config foler

--- lib/hockey_equipment_deals/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../scraper.rb'
-require_relative '../hockey_equipment_deals.rb'
-require_relative '../top_seller.rb'
-require_relative '../clearance.rb'
-require_relative '../menu.rb'
