e8df47715a247217a98fbebd6b59940afae48f48
Michael Carroll <carrollm2@gmail.com>
2019-12-28 21:07:48 +0000

    moved top_seller.rb to lib/hockey_equipment_deals

---
+++ lib/hockey_equipment_deals/top_seller.rb
@@ -0,0 +1,36 @@
+require_relative '../../config/environment'
+require_relative '../hockey_equipment_deals'
+
+class HockeyEquipmentDeals::TopSeller
+
+  include HockeyEquipmentDeals::InstanceMethods
+
+  attr_accessor :name, :sales_price, :description_link, :store
+
+  @@all = []
+
+  def initialize(hockey_deal_hash)
+    @name = self.set_name(hockey_deal_hash)
+    @sales_price = hockey_deal_hash[:sales_price]
+    @description_link = self.set_description_link(hockey_deal_hash)    
+    @store = self.set_store(hockey_deal_hash[:store])
+
+    @@all << self
+  end
+
+  def self.create_from_collection(hockey_deal_array)
+    hockey_deal_array.each_with_index do |hockey_deal, index|
+      new_hockey_deal = HockeyEquipmentDeals::TopSeller.new(hockey_deal_array[index])
+    end
+  end  
+
+  def self.all
+    @@all
+  end  
+
+  def print_message
+    puts "This #{@name} is a top seller available to you at $#{@sales_price}. Check here, #{self.description_link} for more details"
+  end
+
+
+end
